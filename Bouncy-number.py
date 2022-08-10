76c6ff01e7b876ee665fde88f84ec31b2d72fb7e
JeanLucasF <110963910+JeanLucasF@users.noreply.github.com>
2022-08-10 03:26:04 -0500

    Update Bouncy-number.py

--- Bouncy-number.py
+++ Bouncy-number.py
@@ -1,72 +1,42 @@
-from itertools import count, tee
-
-
-class Bouncy:
-    def __init__(self, porcentage):
-        """
-        print the number bouncy
-        :type porcentage: int -> this is porcentage of the bouncy
-        """
-        nums = count(1)
-        rebound = self.sum_number(map(lambda number: float(self.is_rebound(number)), count(1)))
-        bouncy = next(
-            (
-                number
-                for number, number_b in zip(nums, rebound)
-                if number_b / number == (porcentage / 100)
-            )
-        )
-        print(bouncy)
-
-    def pairs(self, iterable):
-        """
-        return a list convert map, produces new list
-        :type number: int
-        """
-        # tee() get iterator independent (default 2) with a input
-        a, b = tee(iterable)
-        # next() return next element in the secuence
-        next(b, None)
-        # zip() return new iterator
-        return zip(a, b)
-
-    def digits(self, number):
-        """
-        return a list convert map, produces new list
-        :type number: int
-        """
-        return list(map(int, str(number)))
-
-    def increase(self, number):
-        """
-        return the elements as long as the previous number is less than or equal to the current one
-        :type number: int
-        """
-        return all(prev <= curr for prev, curr in self.pairs(self.digits(number)))
-
-    def decrease(self, number):
-        """
-        return the elements as long as the previous number is greater than or equal to the current one
-        :type number: int
-        """
-        return all(prev >= curr for prev, curr in self.pairs(self.digits(number)))
-
-    def is_rebound(self, number):
-        """
-        return the elements is rebound
-        :type number: int
-        """
-        return not self.increase(number) and not self.decrease(number)
-
-    def sum_number(self, iterable):
-        """
-        return a element sum total
-        :type iterable: list
-        """
-        total = 0
-        for element in iterable:
-            total += element
-            yield total
-
-
-test = Bouncy(99)
+#Creamos la clase bouncy
+class Bouncy1:
+    def __init__(self, porcentaje):
+        self.porcentaje=porcentaje
+    #Función para determinar si un número es Bouncy o no.
+    def Es_bouncy(self,num):
+        #se pasa el numero a string
+        n=str(num)
+        #este comando convierte el string en una lista organizada en forma ascendente
+        a=sorted(n)
+        #invierte la lista anterior para tenerla el número en una lista descedente
+        d=a[::-1]
+        #convertimos el numero original a una lista
+        nu=list(n)
+        #si el numero original no es igual a su forma ascende o descente entonces es buncy
+        '''Por ejemplo:1234 es ascendete, cuando lo organizamos en forma "asecendete" 
+        por logica sigue igual, por tanto si los comparamos van a ser los mismo. '''
+        if(nu!=a and nu!=d):
+            return(True)
+        else:
+            return(False)
+    #Función que calcula el ultimo numero Bouncy dado un porcentaje
+    def Numero(self):
+        #variables iniciales
+        i=0 
+        b=0 
+        bol=True
+        while bol:
+            i=i+1#contador
+            if(self.Es_bouncy(i)): #si es Bouncy va contado
+                b=b+1#contador
+            #Calcula el porcentaje en cada ciclo por regla de tres
+            porcen=(b*100)/i
+            #verifica si el porcentaje aun no es el indicado, esto permite que se repita 
+            #el ciclo
+            if(porcen!=self.porcentaje):
+                bol=True
+            else:
+                bol=False
+        print(i)  
+b=Bouncy(99)
+b.Numero()
